90ffb2ff71fe206c84662e7ce86dae76f69c2b1a
alkly <109037181+Alkly47@users.noreply.github.com>
2024-05-26 16:05:36 -0600

    Expand and distribute classes

--- CSPS/main.h
+++ CSPS/main.h
@@ -3,24 +3,16 @@
 
 CSPS PowerSupply(0x5F, 0x57, true);
 
-class CSPSPower : public PollingComponent {
+class CSPStext : public PollingComponent {
   public:
-    TextSensor *name = new TextSensor();
-    TextSensor *ct = new TextSensor();
-    Sensor *fan_speed = new Sensor();
-    Sensor *temp1 = new Sensor();
-    Sensor *temp2 = new Sensor();
-    Sensor *power_in = new Sensor();
-    Sensor *power_out = new Sensor();
-    Sensor *current_out = new Sensor();
-    Sensor *current_in = new Sensor();
-    Sensor *voltage_out = new Sensor();
-    Sensor *voltage_in = new Sensor();
-
-    CSPSPower(): PollingComponent(2000) { }
-
+  	TextSensor *spn = new TextSensor();
+  	TextSensor *mfg = new TextSensor();
+    TextSensor *mfr = new TextSensor();
+  	TextSensor *name = new TextSensor();
+    TextSensor *okn = new TextSensor();
+  	TextSensor *ct = new TextSensor();
+    CSPStext(): PollingComponent(60000) { }
     void setup() override {
-
       ESP_LOGD("Power Supply", "Spare Part No: %s", PowerSupply.getSPN().c_str());
       ESP_LOGD("Power Supply", "Manufacture Date: %s", PowerSupply.getMFG().c_str());
       ESP_LOGD("Power Supply", "Manufacturer: %s", PowerSupply.getMFR().c_str());
@@ -28,23 +20,78 @@ class CSPSPower : public PollingComponent {
       ESP_LOGD("Power Supply", "Option Kit No: %s", PowerSupply.getOKN().c_str());
       ESP_LOGD("Power Supply", "CT Date Codes: %s", PowerSupply.getCT().c_str());
     }
-
     void update() override {
       name->publish_state(PowerSupply.getName().c_str());
+      mfr->publish_state(PowerSupply.getMFR().c_str());
+      mfg->publish_state(PowerSupply.getMFG().c_str());
       ct->publish_state(PowerSupply.getCT().c_str());
-      
-      fan_speed->publish_state(PowerSupply.getFanRPM());
-      temp1->publish_state(PowerSupply.getTemp1());
-      temp2->publish_state(PowerSupply.getTemp2());
-      power_out->publish_state(PowerSupply.getOutputPower());
+      spn->publish_state(PowerSupply.getSPN().c_str());
+      okn->publish_state(PowerSupply.getOKN().c_str());
+
+    }
+};   
+class CSPSOther : public PollingComponent {
+  public:
+    Sensor *rt = new Sensor();
+    Sensor *pao = new Sensor();
+    CSPSOther(): PollingComponent(60000) { }
+    void setup() override {
+    }
+    void update() override {
+      rt->publish_state(PowerSupply.getRunTime());
+      pao->publish_state(PowerSupply.getPeakAmpsOut());
+    }
+};    
+class CSPSPowerIn : public PollingComponent {
+  public:
+    Sensor *power_in = new Sensor();
+    Sensor *current_in = new Sensor();
+    Sensor *voltage_in = new Sensor();
+    CSPSPowerIn(): PollingComponent(2500) { }
+    void setup() override {
+    }
+    void update() override {
       power_in->publish_state(PowerSupply.getInputPower());
-      current_out->publish_state(PowerSupply.getOutputCurrent());
       current_in->publish_state(PowerSupply.getInputCurrent());
-      voltage_out->publish_state(PowerSupply.getOutputVoltage());
       voltage_in->publish_state(PowerSupply.getInputVoltage());
     }
-    
+};
+class CSPSPowerOut : public PollingComponent {
+  public:
+    Sensor *power_out = new Sensor();
+    Sensor *current_out = new Sensor();
+    Sensor *voltage_out = new Sensor();
+    CSPSPowerOut(): PollingComponent(2250) { }
+    void setup() override {
+    }
+    void update() override {
+      power_out->publish_state(PowerSupply.getOutputPower());
+      current_out->publish_state(PowerSupply.getOutputCurrent());
+      voltage_out->publish_state(PowerSupply.getOutputVoltage());
+    }
+};
+class CSPSFan : public PollingComponent {
+  public:
+    Sensor *fan_speed = new Sensor();
+    CSPSFan(): PollingComponent(2000) { }
+    void setup() override {
+    }
+    void update() override {
+      fan_speed->publish_state(PowerSupply.getFanRPM());
+    }
     void set_fan_rpm(float state) {
       PowerSupply.setFanRPM(state);
+    }
+};
+class CSPSTemp : public PollingComponent {
+  public:
+    Sensor *temp1 = new Sensor();
+    Sensor *temp2 = new Sensor();
+    CSPSTemp(): PollingComponent(15000) { }
+    void setup() override {
+    }
+    void update() override {
+      temp1->publish_state(PowerSupply.getTemp1());
+      temp2->publish_state(PowerSupply.getTemp2());
     }
 };
